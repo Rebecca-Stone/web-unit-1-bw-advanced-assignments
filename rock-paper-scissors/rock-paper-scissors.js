f5c371f339e4c847a696b0f54e609d154e19aaf8
Rebecca Stone <you@example.com>
2022-02-01 23:50:47 -0600

    finished JavaScript on rock, paper, scissors game. Still need to update the css

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,42 +1,59 @@
 // you need to create selectors to update the scores and the moves being displayed
 const scoreKeepUser = document.querySelector(".scoreKeepUser");
 const scoreKeepComp = document.querySelector(".scoreKeepComp");
-const userChoice = document.querySelector("#left");
-const computerChoice = document.querySelector("#right");
+const win = document.querySelector(".win");
+const lose = document.querySelector(".lose");
+const tie = document.querySelector(".tie");
+const userDisplay = document.querySelector("#left");
+const computerDisplay = document.querySelector("#right");
+
 // you also need a selector for your move selection buttons
 const rockUser = document.querySelector("#rock");
 const paperUser = document.querySelector("#paper");
 const scissorsUser = document.querySelector("#scissors");
-const user = "";
-
+let user = "";
+let computerChoice = "";
+let computer = "";
+let userCount = 0;
+let compCount = 0;
 // you need to update the display area to show your move for each turn
 
 rockUser.addEventListener("click", function () {
-  userChoice.className = "fas fa-hand-rock";
+  computerChoice = Math.random();
+  getComputerChoice(computerChoice);
+  userDisplay.className = "fas fa-hand-rock";
   user = "rock";
+  game(user, computer);
 });
 
 paperUser.addEventListener("click", function () {
-  userChoice.className = "fas fa-hand-paper";
+  computerChoice = Math.random();
+  getComputerChoice(computerChoice);
+  userDisplay.className = "fas fa-hand-paper";
   user = "paper";
+  game(user, computer);
 });
 
 scissorsUser.addEventListener("click", function () {
-  userChoice.className = "fas fa-hand-scissors";
+  computerChoice = Math.random();
+  getComputerChoice(computerChoice);
+  userDisplay.className = "fas fa-hand-scissors";
   user = "scissors";
+  game(user, computer);
 });
 // you need a random selection function for your computer to pick a move each turn
-let computer = Math.random();
 
-if (computer <= 0.34) {
-  computerChoice.className = "fas fa-hand-rock";
-  computer = "rock";
-} else if (computer <= 0.67) {
-  computerChoice.className = "fas fa-hand-paper";
-  computer = "paper";
-} else if (computer > 0.67) {
-  computerChoice.className = "fas fa-hand-scissors";
-  computer = "scissors";
+function getComputerChoice(computerChoice) {
+  if (computerChoice <= 0.34) {
+    computerDisplay.className = "fas fa-hand-rock";
+    computer = "rock";
+  } else if (computerChoice <= 0.67) {
+    computerDisplay.className = "fas fa-hand-paper";
+    computer = "paper";
+  } else if (computerChoice > 0.67) {
+    computerDisplay.className = "fas fa-hand-scissors";
+    computer = "scissors";
+  }
 }
 // you need a function for win/loss/tie scenarios
 
@@ -44,14 +61,38 @@ if (computer <= 0.34) {
 
 function game(user, computer) {
   if (user === computer) {
-    return `it's a tie`;
+    tie.style.display = "block";
+    win.style.display = "none";
+    lose.style.display = "none";
+    scoreKeepUser.innerHTML = userCount;
+    scoreKeepComp.innerHTML = compCount;
   } else if (user === "rock" && computer === "scissors") {
-    return `you win!`;
+    win.style.display = "block";
+    lose.style.display = "none";
+    tie.style.display = "none";
+    userCount++;
+    scoreKeepUser.innerHTML = userCount;
+    scoreKeepComp.innerHTML = compCount;
   } else if (user === "paper" && computer === "rock") {
-    return `you win!`;
+    win.style.display = "block";
+    lose.style.display = "none";
+    tie.style.display = "none";
+    userCount++;
+    scoreKeepUser.innerHTML = userCount;
+    scoreKeepComp.innerHTML = compCount;
   } else if (user === "scissors" && computer === "paper") {
-    return `you win!`;
+    win.style.display = "block";
+    lose.style.display = "none";
+    tie.style.display = "none";
+    userCount++;
+    scoreKeepUser.innerHTML = userCount;
+    scoreKeepComp.innerHTML = compCount;
   } else {
-    return `you lose!`;
+    lose.style.display = "block";
+    win.style.display = "none";
+    tie.style.display = "none";
+    compCount++;
+    scoreKeepUser.innerHTML = userCount;
+    scoreKeepComp.innerHTML = compCount;
   }
 }
